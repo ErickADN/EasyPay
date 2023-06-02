0e83fc41306c4dca3003f2cd8f62c6a7493c0e9f
AngyrYT <nazer1569@gmail.com>
2023-06-01 20:40:01 -0500

    Prueba 1

--- EasyPay/IService1.cs
+++ EasyPay/IService1.cs
@@ -8,6 +8,7 @@ using System.Text;
 
 namespace EasyPay
 {
+    //ESTA ES UNA PRUEBA
     // NOTA: puede usar el comando "Rename" del menú "Refactorizar" para cambiar el nombre de interfaz "IService1" en el código y en el archivo de configuración a la vez.
     [ServiceContract]
     public interface IService1
